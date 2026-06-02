ac517456f5a1b073d96f2470a2d9ce008309594a
rahuljraj157 <rahuljraj157@gmail.com>
2026-06-02 13:40:26 +0530

    remove deprecated eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ disables lint errors during build
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
-
